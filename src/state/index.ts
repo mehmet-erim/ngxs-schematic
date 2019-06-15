58f3384f87d1e3dd8908fe007fb708b94fd3b8ee
mehmet-erim <bilgi@mehmeterim.com>
2019-06-16 02:18:55 +0300

    able to using template

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,27 +1,28 @@
-import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  url,
+  apply,
+  template,
+  mergeWith
+} from "@angular-devkit/schematics";
+import { strings } from "@angular-devkit/core";
 
 // Instead of `any`, it would make sense here to get a schema-to-dts package and output the
 // interfaces so you get type-safe options.
 export default function(options: any): Rule {
   // The chain rule allows us to chain multiple rules and apply them one after the other.
   return (tree: Tree, context: SchematicContext) => {
-    // Show the options for this Schematics.
-    // The logging here is at the discretion of the tooling. It can be ignored or only showing
-    // info/warnings/errors. If you use console.log() there is not guarantee it will be
-    // propagated to a user in any way (for example, an IDE running this schematic might
-    // have a logging window but no support for console.log).
-    context.logger.info("My Schematic: " + JSON.stringify(options));
+    const sourceTemplates = url("./files");
 
-    // Create a single file. This is the simplest example of transforming the tree.
-    // If a file with that name already exists, the generation will NOT fail until the tool
-    // is trying to commit this to disk. This is because we allow you to work on what is
-    // called a "staging" area, and only finalize those changes when the schematics is
-    // done. This allows you to create files without having to verify if they exist
-    // already, then rename them later. Templating works in a similar fashion.
-    tree.create("hello", "world");
+    const sourceParametrizedTemplates = apply(sourceTemplates, [
+      template({
+        ...options,
+        ...strings
+      })
+    ]);
 
-    // At the end, you can either return a Tree (that will be used), or an observable of a
-    // Tree (if you have some asynchronous tasks).
-    return tree;
+    return mergeWith(sourceParametrizedTemplates);
   };
 }
