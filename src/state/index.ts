7a43f06923e8ff849bd0b682ed0bae650e766b9b
mehmet-erim <bilgi@mehmeterim.com>
2019-06-16 02:19:11 +0300

    remove comment lines

--- src/state/index.ts
+++ src/state/index.ts
@@ -9,10 +9,7 @@ import {
 } from "@angular-devkit/schematics";
 import { strings } from "@angular-devkit/core";
 
-// Instead of `any`, it would make sense here to get a schema-to-dts package and output the
-// interfaces so you get type-safe options.
 export default function(options: any): Rule {
-  // The chain rule allows us to chain multiple rules and apply them one after the other.
   return (tree: Tree, context: SchematicContext) => {
     const sourceTemplates = url("./files");
 
