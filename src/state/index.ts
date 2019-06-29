99123407c40a03d14175868fcc513c13a64c3016
mehmet-erim <bilgi@mehmeterim.com>
2019-06-29 15:19:06 +0300

    Add prettierrc Organize paths libraries and apps

--- src/state/index.ts
+++ src/state/index.ts
@@ -9,62 +9,60 @@ import {
   move,
   forEach,
   FileEntry,
-  SchematicsException
-} from "@angular-devkit/schematics";
-import { strings } from "@angular-devkit/core";
-import { Schema } from "./schema";
-import { parseName, buildDefaultPath } from "../utils";
+  SchematicsException,
+} from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
+import { Schema } from './schema';
+import { parseName, buildDefaultPath } from '../utils';
 
 export default function(options: Schema): Rule {
   return (tree: Tree, context: SchematicContext) => {
-    const workspaceConfigBuffer = tree.read("angular.json");
+    const workspaceConfigBuffer = tree.read('angular.json');
     if (!workspaceConfigBuffer) {
-      throw new SchematicsException("Not an ANGULAR CLI workspace");
+      throw new SchematicsException('Not an ANGULAR CLI workspace');
     }
 
     const workspaceConfig = JSON.parse(workspaceConfigBuffer.toString());
     const projectName = options.project || workspaceConfig.defaultProject;
     const project = workspaceConfig.projects[projectName];
-
+    const isLibrary = project.projectType === 'library';
     const defaultProjectPath = buildDefaultPath(project);
 
     const parsedPath = parseName(defaultProjectPath, options.name);
 
     const { name, path } = parsedPath;
 
-    console.log(parsedPath);
-
     if (!options.path) {
-      options.path = "/src/app/store/";
+      options.path = `${path}/${isLibrary ? 'lib/' : 'store/'}`;
     }
 
-    const sourceTemplates = url("./files");
+    const sourceTemplates = url('./files');
 
     const sourceParametrizedTemplates = apply(sourceTemplates, [
       template({
         ...options,
-        ...strings
+        ...strings,
       }),
       move(options.path),
       forEach((file: FileEntry) => {
         if (!tree.exists(file.path)) return file;
 
-        if (file.path.substr(file.path.lastIndexOf("/") + 1) === "index.ts") {
+        if (file.path.substr(file.path.lastIndexOf('/') + 1) === 'index.ts') {
           const content: Buffer | null = tree.read(file.path);
-          // console.log((content as Buffer).toString());
+
           if (content && !content.toString().includes(options.name)) {
-            let updatedContent = "";
+            let updatedContent = '';
             updatedContent = (content.toString() + file.content.toString())
-              .split("\n")
+              .split('\n')
               .filter(line => !!line)
               .sort()
-              .join("\n");
+              .join('\n');
 
             tree.overwrite(file.path, updatedContent);
           }
         }
         return null;
-      })
+      }),
     ]);
 
     return mergeWith(sourceParametrizedTemplates);
