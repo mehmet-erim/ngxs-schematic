4a515124549fab8b14998b8303f55a06f783cfe6
mehmet-erim <bilgi@mehmeterim.com>
2019-07-12 10:32:18 +0300

    refactor: remove desc property from action

--- src/state/files/actions/__name@dasherize__.actions.ts
+++ src/state/files/actions/__name@dasherize__.actions.ts
@@ -1,5 +1,4 @@
 export class <%= classify(name) %>Action {
   static readonly type = '[<%= classify(name) %>] Action';
-  static readonly desc = '<%= dasherize(name) %> description';
   constructor(public readonly payload?: any) { }
 }
